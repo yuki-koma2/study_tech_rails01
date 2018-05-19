63b6a65f6c864c1971cc9a6169be43c336e9f1dc
EC2 Default User <ec2-user@ip-172-31-15-189.us-east-2.compute.internal>
2018-05-19 05:44:37 +0000

    fin comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,12 @@ class UsersController < ApplicationController
         #current_user
         # => nil
         #Loginしないと値は反映されない。
+        
+        user = User.find(params[:id])
         @nickname = current_user.nickname
         #binding.pry
         #@tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
-        @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+        # @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+        @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
     end
 end
