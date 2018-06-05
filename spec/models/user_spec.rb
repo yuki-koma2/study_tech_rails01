acb6998593b6fa38ba4ff3b41d04d06291aad29c
EC2 Default User <ec2-user@ip-172-31-15-189.us-east-2.compute.internal>
2018-06-05 05:20:00 +0000

    comment out test code but error...

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
-describe User do 
-    describe '#create' do 
-        it "is incalid without a nickname" do 
-        end
+describe User do
+  describe '#create' do
+    it "is invalid without a nickname" do
+    #  user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+    #  user.valid?
+    #  expect(user.errors[:nickname]).to include("can't be blank")
     end
+  end
 end
