e9109492b6e6f97c4ee96078be7fb6fa2d644c61
yuki komatsu <yuki.k.lion@gmail.com>
2018-06-09 11:11:23 +0900

    一旦コメントアウトで消しておく。

--- pictweet/spec/models/user_spec.rb
+++ pictweet/spec/models/user_spec.rb
@@ -1,7 +1,8 @@
-require 'rails_helper'
-describe User do
-  describe '#create' do
-    it "is invalid without a nickname" do
-    end
-  end
-end
+# require 'rails_helper'
+# describe User do
+#   describe '#create' do
+#     it "is invalid without a nickname" do
+#
+#     end
+#   end
+# end
