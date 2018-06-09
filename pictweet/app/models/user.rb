a2986aaa3cb4136522c5673decefcda3fdc2738b
yuki komatsu <yuki.k.lion@gmail.com>
2018-06-09 11:10:03 +0900

    単体テストの記述
    なぜかうまく行かないので、一旦commit。user_spec.rbの記述が間違っているのか？？

--- pictweet/app/models/user.rb
+++ pictweet/app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
   # has_many  :comments
-  has_many :comments 
+  has_many :comments
+  #バリデーションの追加
+  validates :nickname, presence: true, length: { maximum: 6 }
 end
