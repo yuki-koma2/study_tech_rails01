4302e5af3776984bd2e83d8927f122d1beb8fe58
yuki komatsu <yuki.k.lion@gmail.com>
2018-06-09 10:59:07 +0900

    add gemfile
    単体テスト用のコードを追記していく

--- pictweet/db/schema.rb
+++ pictweet/db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20180519045557) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "nickname",               limit: 255
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
@@ -43,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180519045557) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
